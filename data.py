dd8dd54cb739862ad4de5782cdbe4714f3b860a6
Manish Gollapally <manishgollapally@gmail.com>
2025-04-05 18:43:00 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,176 @@
+data = [
+    {
+        'name': 'BMW',
+        'type': 'automobile company',
+        'nation': 'German',
+        'searches': 1250000
+    },
+    {
+        'name': 'BAAHUBALI',
+        'type': 'Film',
+        'nation': 'Indian',
+        'searches': 6500
+    },
+    {
+        'name': 'Justin Beiber',
+        'type': 'Song writer and singer',
+        'nation': 'American',
+        'searches': 36000
+    },
+    {
+        'name': 'Donald Trump',
+        'type': 'President',
+        'nation': "American",
+        'searches': 7800
+    },
+    {
+        'name': 'Instagram',
+        'type': 'Social media platform',
+        'nation': 'American',
+        'searches': 1530000
+    },
+    {
+        'name': 'Statue of Liberty',
+        'type': 'Statue in United States',
+        'nation': 'American',
+        'searches': 256000
+    },
+    {
+        'name': 'Bali',
+        'type': 'Vacation destination',
+        'nation': 'Indonasian',
+        'searches': 320000
+    },
+    {
+        'name': 'Apple',
+        'type': 'Tech company',
+        'nation': 'American',
+        'searches': 467000
+    },
+    {
+        'name': 'La Casa de Papel',
+        'type': 'Web Series',
+        'nation': 'Spanish',
+        'searches': 900
+    },
+    {
+        'name': 'Eiffel Tower',
+        'type': 'landmark',
+        'nation': 'France',
+        'searches': 1200000
+    },
+    {
+        'name': 'Great Wall of China',
+        'type': 'monument',
+        'nation': 'China',
+        'searches': 950000
+    },
+    {
+        'name': 'Statue of Liberty',
+        'type': 'monument',
+        'nation': 'USA',
+        'searches': 800000
+    },
+    {
+        'name': 'Taj Mahal',
+        'type': 'monument',
+        'nation': 'India',
+        'searches': 750000
+    },
+    {
+        'name': 'Colosseum',
+        'type': 'landmark',
+        'nation': 'Italy',
+        'searches': 700000
+    },
+    {
+        'name': 'Machu Picchu',
+        'type': 'historical site',
+        'nation': 'Peru',
+        'searches': 650000
+    },
+    {
+        'name': 'Louvre Museum',
+        'type': 'museum',
+        'nation': 'France',
+        'searches': 1100000
+    },
+    {
+        'name': 'Pyramids of Giza',
+        'type': 'historical site',
+        'nation': 'Egypt',
+        'searches': 600000
+    },
+    {
+        'name': 'Amazon Rainforest',
+        'type': 'natural wonder',
+        'nation': 'Brazil',
+        'searches': 550000
+    },
+    {
+        'name': 'Grand Canyon',
+        'type': 'natural wonder',
+        'nation': 'USA',
+        'searches': 500000
+    },
+    {
+      'name': 'Cristiano Ronaldo',
+      'type': 'Athlete',
+      'nation': 'Portugal',
+      'searches': 1800000
+    },
+    {
+      'name': 'Taylor Swift',
+      'type': 'Musician',
+      'nation': 'USA',
+      'searches': 2000000
+    },
+    {
+      'name': 'Albert Einstein',
+      'type': 'Scientist',
+      'nation': 'Germany',
+      'searches': 780000
+    },
+    {
+      'name': 'Mona Lisa',
+      'type': 'Painting',
+      'nation': 'France',
+      'searches': 1300000
+    },
+    {
+        'name': 'Bitcoin',
+        'type': 'Cryptocurrency',
+        'nation': 'Global',
+        'searches': 1500000
+    },
+    {
+        'name': 'ChatGPT',
+        'type': 'AI Model',
+        'nation': 'Global',
+        'searches': 3000000
+    },
+    {
+        'name': 'NASA',
+        'type': 'Space Agency',
+        'nation': 'USA',
+        'searches': 1000000
+    },
+    {
+        'name': 'The Beatles',
+        'type': 'Music Band',
+        'nation': 'UK',
+        'searches': 600000
+    },
+    {
+        'name': 'Avengers Endgame',
+        'type': 'Movie',
+        'nation': 'USA',
+        'searches': 900000
+    },
+    {
+        'name': 'Playstation 5',
+        'type': 'Gaming Console',
+        'nation': 'Japan',
+        'searches': 850000
+    }
+]
